326c9a4cf59abb85e4e0d9f575a94981a949ec66
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2025-02-17 14:00:24 -0500

    Fix a few simulation issues and clean up the code a bit.

--- hello-fluid-simulator/src/main.rs
+++ hello-fluid-simulator/src/main.rs
@@ -9,7 +9,7 @@ use std::{
 
 // Display Constants
 const HEIGHT_FIELD_SIZE: usize = 80;
-const GRAYSCALE_TABLE: [char; 10] = [' ', '.', ':', '-', '=', '+', '*', '#', '%', '@'];
+const GRAYSCALE_TABLE: &[char] = &[' ', '▁', '▂', '▃', '▄', '▅', '▆', '▇', '█'];
 
 // Simulation Constants
 const BOUNDARY_LEFT: f64 = 0.0;
@@ -36,21 +36,22 @@ fn accumulate_wave_to_field(
 ) {
 	let field_size = height_field.len() as isize;
 	let quarter_wave_length = 0.25 * wave_length;
-	let start = (position - quarter_wave_length) as isize * field_size;
-	let end = (position + quarter_wave_length) as isize * field_size;
+	let start = ((position - quarter_wave_length) * field_size as f64) as isize;
+	let end = ((position + quarter_wave_length) * field_size as f64) as isize;
 
-	for i in start..end {
-		let j = if i < 0 {
-			(-i - 1) as usize
-		} else if i >= field_size {
-			(2 * field_size - i - 1) as usize
+	for field_index in start..end {
+		// Boundary Reflection
+		let reflected_field_index = if field_index < 0 {
+			(-field_index - 1) as usize
+		} else if field_index >= field_size {
+			(2 * field_size - field_index - 1) as usize
 		} else {
-			i as usize
+			field_index as usize
 		};
 
-		let distance = (i as f64 + 0.5).abs() / field_size as f64 - position;
+		let distance = ((field_index as f64 + 0.5) / field_size as f64 - position).abs();
 		let height = max_height * 0.5 * ((distance * PI / quarter_wave_length).min(PI).cos() + 1.0);
-		height_field[j] += height;
+		height_field[reflected_field_index] += height;
 	}
 }
 
@@ -67,10 +68,9 @@ fn draw(height_field: &Vec<f64>) {
 	}
 
 	// Clear old prints
-	/*for _ in 0..field_size {
+	for _ in 0..field_size {
 		print!("\x08");
-	}*/
-	println!();
+	}
 
 	// Draw the new buffer
 	print!("{}", buffer);
@@ -79,27 +79,28 @@ fn draw(height_field: &Vec<f64>) {
 
 // Entry Point
 fn main() {
-	const FPS: u32 = 100;
-	const TIME_INTERVAL: f64 = 1.0 / FPS as f64;
+	const SIMULATION_TIME_PERIOD_SECONDS: u32 = 1000;
+	const FRAMES_PER_SECOND: u32 = 120;
+	const FRAME_TIME_INTERVAL: f64 = 1.0 / FRAMES_PER_SECOND as f64;
 
 	let wave_length_x = 0.8;
-	let wave_length_y = 1.2;
 	let max_height_x = 0.5;
+	let wave_length_y = 1.2;
 	let max_height_y = 0.4;
 
-	let mut position_x = 0.0;
-	let mut position_y = 0.0;
+	let mut position_x = BOUNDARY_LEFT;
 	let mut speed_x = 1.0;
+	let mut position_y = BOUNDARY_RIGHT;
 	let mut speed_y = -0.5;
 
 	let mut height_field = vec![0.0; HEIGHT_FIELD_SIZE];
 
 	// Main Loop
-	let sleep_duration = Duration::from_secs_f64(TIME_INTERVAL);
-	for _ in 0..1000 {
+	let sleep_duration = Duration::from_secs_f64(FRAME_TIME_INTERVAL);
+	for _ in 0..(SIMULATION_TIME_PERIOD_SECONDS * FRAMES_PER_SECOND) {
 		// March through time
-		update_wave(TIME_INTERVAL, &mut position_x, &mut speed_x);
-		update_wave(TIME_INTERVAL, &mut position_y, &mut speed_y);
+		update_wave(FRAME_TIME_INTERVAL, &mut position_x, &mut speed_x);
+		update_wave(FRAME_TIME_INTERVAL, &mut position_y, &mut speed_y);
 
 		// Clear height field
 		for height in &mut height_field {
